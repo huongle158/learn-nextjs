69e2fb9bffbf2bc93dd9d8ed4b2f58b2f6b353e2
huongle158 <lehuongvt15@gmail.com>
2022-10-07 22:18:26 +0700

    config something

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
 }
-
-module.exports = nextConfig
